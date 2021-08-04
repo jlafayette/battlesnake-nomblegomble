1feca2a48b8f57ac3e3a38b645529dc7083d4b28
Josh LaFayette <joshlafayette@gmail.com>
2021-08-03 23:50:19 -0700

    Update deploy script
    - stop process on remote server and restart once the executable has
      been replaced
    - add invoke format task to format python code

--- tasks.py
+++ tasks.py
@@ -7,24 +7,24 @@ from dotenv import load_dotenv
 from invoke import task
 
 
-
 async def copy_file(path):
     load_dotenv()
-    host = os.environ["HOST"]
-    port = int(os.environ["PORT"])
+    host = os.environ["REMOTE_HOST"]
+    port = int(os.environ.get("REMOTE_PORT", 22))
     username = os.environ["REMOTE_USER"]
     password = os.environ["REMOTE_PASSWORD"]
-    async with asyncssh.connect(host,
-                                port=port,
-                                username=username,
-                                password=password,
-                                known_hosts=None) as conn:
+    async with asyncssh.connect(
+        host, port=port, username=username, password=password, known_hosts=None
+    ) as conn:
+        kill_result = await conn.run(f"kill -9 `pidof ./{path}`")
+        print(f"# kill -9 `pidof ./{path}`\n{kill_result.returncode} {kill_result.stdout}")
         async with conn.start_sftp_client() as sftp:
             is_file = await sftp.isfile(path)
             if is_file:
                 await sftp.remove(path)
             await sftp.put(path)
         await conn.run(f"chmod +x {path}", check=True)
+        await conn.run(f"setsid ./{path} < /dev/zero 2>&1&> /dev/null &")
 
 
 @task
@@ -35,4 +35,9 @@ def deploy(ctx):
     try:
         asyncio.get_event_loop().run_until_complete(copy_file(f))
     except (OSError, asyncssh.Error) as exc:
-        sys.exit('SSH connection failed: ' + str(exc))
+        sys.exit("SSH connection failed: " + str(exc))
+
+
+@task
+def format(ctx):
+    ctx.run("black -l 100 tasks.py")
