b46b400f563a1e12aadf8cadd157cf3e5e6cca51
Josh LaFayette <joshlafayette@gmail.com>
2021-08-03 23:54:49 -0700

    Personalize the snake

--- logic.go
+++ logic.go
@@ -19,10 +19,10 @@ func info() BattlesnakeInfoResponse {
 	log.Println("INFO")
 	return BattlesnakeInfoResponse{
 		APIVersion: "1",
-		Author:     "",        // TODO: Your Battlesnake username
-		Color:      "#888888", // TODO: Personalize
-		Head:       "default", // TODO: Personalize
-		Tail:       "default", // TODO: Personalize
+		Author:     "jlafayette",
+		Color:      "#3399ff",
+		Head:       "caffeine",
+		Tail:       "round-bum",
 	}
 }
 
